a2b84f08a1e59a430a2883ff8eefdfef90c53a1a
aa6773 <aa6773@srmist.edu.in>
2025-04-28 10:30:00 +0530

    java8

---
+++ day-6/java8/functionalInterfaces/ConsumerInterface.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerInterface {
+    public static void main(String[] args) {
+       List<Integer> list= Arrays.asList(1,2,3,4);
+
+        Consumer<Integer> obj1= x-> System.out.println(x);
+        obj1.accept(5);
+        Consumer<List<Integer>> printList=x->{
+        for(int i:x){
+            System.out.print(i+" ");
+            }
+        };
+        printList.accept(list);
+
+        list.forEach(x-> System.out.println(x));
+        list.forEach(System.out::println);
+    }
+}
